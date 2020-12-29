246f405b3ce39cb64f2828f7164691331874bcb3
duo.zhou <duo.zhou@appshahe.com>
2020-12-29 18:08:47 +0800

    add future rocksdb_test

--- src/common/time/time_test.cpp
+++ src/common/time/time_test.cpp
@@ -1,13 +1,18 @@
 
-#include <time.h>
+#include <ctime>
+#include <chrono>
 #include <iostream>
 
 
 using namespace std;
 
 int main() {
-    std::cout << sizeof(time_t) << std::endl;
-    std::cout << difftime(2, 1) << std::endl;
+    std::cout << "sizeof(time_t): " << sizeof(time_t) << std::endl;
+    std::cout << "difftime(2, 1): " << difftime(2, 1) << std::endl;
+
+    std::chrono::milliseconds msTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch());
+    std::cout << "time in ms: " << msTime.count() << std::endl;
 
     return 0;
 }
