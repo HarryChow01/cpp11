f8bf4ead92c80e0313ce4c00a3c20cd43be98040
harrychow01 <harrychow01@163.com>
2021-01-04 11:14:41 +0800

    rocksdb

--- src/rocksdb_test.cpp
+++ src/rocksdb_test.cpp
@@ -2,31 +2,40 @@
 // Created by Chow on 2020/12/27.
 //
 
+#include <string>
+#include <vector>
 #include <iostream>
+#include <chrono>
 
 #include "rocksdb/db.h"
 
 int main(int argc, char* argv[]) {
+	const size_t kKeyCount = 1024;
+	const size_t kBlockSize = 1024 * 1024;
     rocksdb::DB* db;
     rocksdb::Options options;
     options.create_if_missing = true;
     rocksdb::Status status = rocksdb::DB::Open(options, "./testdb", &db);
     std::cout << "rocksb open status: " << status.ok() << std::endl;
 
-    std::string sKey("key-one");
-    std::string sValue("Value Some");
+    std::string sKey("key1");
+    std::string sValue("value1");
 
-    status = db->Put(rocksdb::WriteOptions(), sKey, sValue);
-    if (status.ok()) {
-        std::cout << "put key ok" << std::endl;
-        std::string rValue;
-        rocksdb::Status s = db->Get(rocksdb::ReadOptions(), sKey, &rValue);
-        if (s.ok()) {
-            std::cout << "read from rocksdb success: " << rValue << std::endl;
-        }
-    } else {
-        std::cout << "put key error, with status: " << status.code() << std::endl;
-    }
+	rocksdb::WriteBatch batch;
+	rocksdb::WriteOptions writeOptions;
+	
+	for (int i = 0; i < kKeyCount; ++i) {
+		std::string key = std::to_string(i + 1);
+		std::string value(kBlockSize, 'a');
+		batch.Put(key, value);
+	}
+
+	auto beginTime = std::chrono::high_resolution_clock::now();
+	db->Write(writeOptions, &batch);
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto elapsedTime1 = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime);
+	std::cout << "batch.Data().size(): " << batch.Data().size() << std::endl;
+	std::cout << "db->Write time in ms: " << elapsedTime1.count() << std::endl;
 
     db->Close();
 
