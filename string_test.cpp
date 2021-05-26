096948c1800879f2553693cea31509fd887794d6
duo.zhou <duo.zhou@appshahe.com>
2021-05-26 18:20:52 +0800

    map相关

--- string_test.cpp
+++ string_test.cpp
@@ -5,6 +5,7 @@
 #include <map>
 
 std::map<std::string, std::shared_ptr<std::string>> gMap;
+std::map<std::string, std::string> gMap2;
 
 void string_test1() {
     std::string str1 = "abc";
@@ -17,12 +18,16 @@ void string_test1() {
     std::shared_ptr<std::string> pStr = std::make_shared<std::string>(str2);
     std::cout << "*pStr: " << *pStr << std::endl;
     gMap.emplace("aa", pStr);
+    gMap2.emplace("aa", str2);
 }
 
 void string_test2() {
     auto it = gMap.find("aa");
     const auto& str1 = *(it->second);
     std::cout << "str1 address: " << &str1 << ", str1 data address: " << static_cast<const void *>(str1.data()) << std::endl;
+    auto it2 = gMap2.find("aa");
+    const auto& str2 = it2->second;
+    std::cout << "str2 address: " << &str2 << ", str2 data address: " << static_cast<const void *>(str2.data()) << std::endl;
 }
 
 int main() {
